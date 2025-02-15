6031444dec76a67f637fe796698be2826e224415
branden kennedy <brandenk88@gmail.com>
2025-02-15 12:41:13 -0800

    launch indicator implemented

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,6 +5,7 @@ public class BallController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float force = 1f;
     [SerializeField] private Transform ballAnchor;
+    [SerializeField] private Transform launchIndicator;
     [SerializeField] private InputManager inputManager;
     private Rigidbody ballRB;
     private bool isBallLaunched;
@@ -23,6 +24,7 @@ public class BallController : MonoBehaviour
         isBallLaunched = true;
         transform.parent = null;
         ballRB.isKinematic = false;
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse); 
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse); 
+        launchIndicator.gameObject.SetActive(false);
     }
 }
