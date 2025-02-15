c121246e8089862cde090a7cd4cf67058d0a7b88
branden kennedy <brandenk88@gmail.com>
2025-02-15 11:55:16 -0800

    Gutters now working properly, implemented
    gutters are now implemented and actually working! (yay)

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -10,7 +10,7 @@ public class BallController : MonoBehaviour
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
-
+        
         inputManager.OnSpacePressed.AddListener(LaunchBall);
     }
 
